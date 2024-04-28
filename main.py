1b7a3a0a8ba50a768d718a6d2beca8fd2550f9b6
kolia <koliaparshin@yandex.ru>
2024-04-28 05:13:32 +0300

    e

--- main.py
+++ main.py
@@ -158,6 +158,8 @@ def admin_federal_add_event_page():
         data = request.form
         if data["full_name"] == None or data["full_name"] == '':
             return render_template('admin_add_federal_event.html', error='Поле "Полное название мероприятия" обязательно')
+        if data['custom_url'] == None or data['custom_url'] == '':
+            return render_template('admin_add_federal_event.html', error='Поле "Ссылкая мероприятия" обязательно')
         # checkpoint = {"date", "description"}
         # track = {"title", "direction", "description", "url"}
         # partners = {"title", "image_url"}
